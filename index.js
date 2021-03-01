2f12b14a6d5cf92f0b389f85942acb4b0738d58a
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-01 18:34:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { token } = require('./config.json');
-const db = require('quick.db');
-const { defaultCipherList } = require('constants');
-const { aliases } = require('./commands/avatar');
+const { channel } = require('./config.json');
 const { ShardingManager } = require('discord.js');
+
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -20,32 +19,7 @@ for (const file of commandFiles) {
 client.once('ready', async () => {
 	console.log('Ready!');
         
-	let i = 0
-	client.user.setActivity(`+help || Serving ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users || Serving ${client.guilds.cache.size} servers`)
-	setInterval(() => client.user.setActivity(`+help || Serving ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users || Serving ${client.guilds.cache.size} servers`), 600000);
 	
-});
-
-client.on('message', message => {
-	let prefix = db.get(`prefix_${message.guild.id}`)
-
-	const defaultprefix = '+'
-
-	if(prefix === null) prefix = defaultprefix
-
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	let args = message.content.slice(prefix.length).trim().split(/ +/);
-	let command = args.shift().toLowerCase();
-
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).run(client, message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
 });
 
 client.login(token);
