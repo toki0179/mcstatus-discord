048efd6dc864cb931cd6db14a96d4fef6663cf2d
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-01 18:36:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const { ShardingManager } = require('discord.js');
 
 
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
 
 client.once('ready', async () => {
 	console.log('Ready!');
