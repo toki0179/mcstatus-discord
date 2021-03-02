7aa4eed669fb93a4298f2ed8262d5424ee38cc2e
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-02 14:42:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ const { token } = require('./config.json');
 const { channelid } = require('./config.json');
 const { server } = require('./config.json');
 const util = require('minecraft-server-util');
-
 const client = new Discord.Client();
 
 client.once('ready', async () => {
 	console.log('Ready!');
         client.user.setActivity('Checking Minecraft Server Status')
-	util.status('play.hypixel.net') // port is default 25565
+	let i = 0
+	setInterval(() => , 600000);
+	util.status(server) // port is default 25565
     	.then((response) => {
-        	message.channelid.send(response);
+        	let playersOnline = response.playersOnline
+		let serverHost = response.host
+		let serverPort = response.port
+		let serverMax = response.maxPlayers
+		let serverVer = response.version
+		
+		const embed = new Discord.messageEmbed()
+		.setTitle(`${serverHost} Status: `)
+		.addFields(
+			{ name: `Players Online: `, value: `${playersOnline}` },
+			{ name: `Max players:`, value: `${serverMax}` },
+			{ name: `Server Host: `, value:`${serverHost}:${serverPort}` },
+			{ name: `Version:`, value: `${serverVer}` }
+		)
+		.setFooter('Created by toki#0999')
+		
+		var Msg = client.channels.get(channelid).send(embed);
+			setInterval(() => Msg.edit(embed), 60);
     })
 
 });
