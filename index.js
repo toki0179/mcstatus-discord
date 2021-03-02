f3c9a64ae60d84bd86e80ba7f800457ed76a8132
toki0179 <47860095+toki0179@users.noreply.github.com>
2021-03-02 11:28:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { token } = require('./config.json');
-const { channel } = require('./config.json');
+const { channelid } = require('./config.json');
 const { server } = require('./config.json');
+const util = require('minecraft-server-util');
 
 const client = new Discord.Client();
 
 client.once('ready', async () => {
 	console.log('Ready!');
         client.user.setActivity('Checking Minecraft Server Status')
+	util.status('play.hypixel.net') // port is default 25565
+    	.then((response) => {
+        	message.channelid.send(response);
+    })
+
 });
 
 client.login(token);
